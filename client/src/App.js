84f54a2ed86cb89622cc04e22472658e1f36d0e5
Ritika Malik <ritikamalik100102@gmail.com>
2022-07-03 11:36:46 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
-import Footer from './Components/footer/footer';
-import Contact from './Components/ContactUs/Contact'
 import LandingPage from './pages/landingPage/landingPage';
 
 function App() {
