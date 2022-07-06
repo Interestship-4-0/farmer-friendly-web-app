63bd3dea0c61b28d77cda74eeccd36a9a93d82bf
Ritika Malik <ritikamalik100102@gmail.com>
2022-07-06 19:10:49 +0530

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './pages/landingPage/landingPage';
+import Dashboard from './pages/Dashboard/dashboard';
+import Sell from './pages/Sell/sell';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage/>}></Route>
-    </Routes> 
+    // <Routes>
+    //   <Route path="/" element={<LandingPage/>}></Route>
+    // </Routes> 
+    <>
+    {/* <LandingPage/> */}
+    <Dashboard/>
+    <Sell/>
+    </>
+
   );
 }
 
---
+++ client/src/pages/Dashboard/dashboard.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import './dashboard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "https://kit.fontawesome.com/94cb9e2368.js";
+
+const data = [
+  { Waste: "Wheat Husk", Quantity: 0 },
+  { Waste: "Rice Husk", Quantity: 0 },
+  { Waste: "Corn Bots", Quantity: 0},
+  { Waste: "Tobacco Crop Waste", Quantity: 0},
+  { Waste: "Castor Crop Waste", Quantity: 0},
+  { Waste: "Cotton Plants Left Out", Quantity: 0},
+]
+const Dashboard = () => {
+  return (
+    <div>
+        <center>
+        <div className="container">
+          <div className="table">
+          <table>
+        <tr>
+          <th>WASTE - TYPE</th>
+          <th>QUANTITY OF WASTE</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.Waste}</td>
+              <td>{val.Quantity}</td>
+            </tr>
+          )
+        })}
+      </table>
+          </div>
+          <div className="mainbox">
+          <div className="main">
+           <div className="box1">
+               <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4  className="text">WHEAT HUSK</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        <div className="box2">
+        <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4  className="text">RICE HUSK</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        </div>
+        <div className="main">
+           <div className="box1">
+           <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4  className="text">CORN BOTS</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        <div className="box2">
+        <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4  className="text">TOBACCO CROP WASTE</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        </div>
+        <div className="main">
+           <div className="box1">
+           <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4  className="text">CASTOR CROP WASTE</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        <div className="box2">
+        <div className="header-form">
+                   <h4 style={{marginTop: "0px"}}><i class="fa-solid fa-cannabis" style={{fontSize:"30px"}}></i></h4>
+                   <h4 style={{fontSize:"14px"}} className="text">COTTON PLANTS LEFT OUT</h4>
+                   <h5 className="text2">Quantity : 0</h5>
+                   <button type="button" className="btn">SELL THE WASTE</button>
+              </div>
+        </div>
+        </div>
+        </div>
+       </div>  
+       </center>
+    </div>
+  )
+}
+
+export default Dashboard
